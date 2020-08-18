79f61cad766fcf3deedbe2253f1c2a817a446671
egor <egor23and1@mail.ru>
2020-08-18 20:04:10 +0600

    Initial commit

---
+++ tConverter/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  tConverter
+//
+//  Created by Егор Желтоножский on 4/8/20.
+//  Copyright © 2020 Егор Желтоножский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let temperatureCelsius = Int(round(sender.value))
+        celsiusLabel.text = "\(temperatureCelsius)ºC"
+        
+        let fahrenheitTemperature = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(fahrenheitTemperature)ºF"
+    }
+    
+}
+
